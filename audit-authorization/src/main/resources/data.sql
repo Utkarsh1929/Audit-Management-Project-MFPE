3e6d05785c2320a42dbfe615dbd8cc374f09170e
Utkarsh1929 <utkarsh.4oct29@gmail.com>
2022-07-06 02:22:41 +0530

    Adding the Project

---
+++ audit-authorization/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (1, 'Utkarsh Mishra', 'ut', 'ut000', 'Project-1');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (2, 'K Vinotha', 'kv', 'kv000', 'Project-2');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (3, 'Adem Vedanth Reddy', 'avr', 'avr000', 'Project-3');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (4, 'V Priyadarsini', 'vp', 'vp000', 'Project-4');
